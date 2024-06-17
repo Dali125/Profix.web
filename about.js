599bcbf7301f1d86dd1c297ffa810ed033dc566c
Dali125 <82496809+Dali125@users.noreply.github.com>
2024-06-17 10:04:31 +0200

    more updates

---
+++ about.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    anime({
+        targets: '#abo',
+        keyframes: [
+            {scale: 0},
+            
+            {scale :1}
+        ],
+
+        duration: 2300
+      
+})
+
+  
+})
